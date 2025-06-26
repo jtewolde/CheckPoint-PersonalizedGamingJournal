82afe897344b436c41bc61bdb9caa730c0f06518
Joseph Tewolde <89418498+jtewolde@users.noreply.github.com>
2025-06-26 11:33:02 -0600

    Updated library's page.tsx

--- checkpoint-gamingjournal/src/app/library/page.tsx
+++ checkpoint-gamingjournal/src/app/library/page.tsx
@@ -5,6 +5,7 @@ import Library from "./LibraryPage"
 // Set the page title for library page
 export const metadata: Metadata = {
   title: "Library | CheckPoint"
+}
 
 export default function Page(){
     return <Library />;
