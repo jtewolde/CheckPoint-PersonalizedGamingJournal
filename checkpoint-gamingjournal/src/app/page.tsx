d382038196cf926bfc9d29b36f014ac0a9a1514e
Joseph <jtewold1@msudenver.edu>
2025-08-04 14:22:21 -0600

    Remove unused imports and test out new image to see if vercel deployment works with this image

--- checkpoint-gamingjournal/src/app/page.tsx
+++ checkpoint-gamingjournal/src/app/page.tsx
@@ -13,11 +13,6 @@ import { useState, useEffect } from 'react';
 import classes from './page.module.css';
 import { useAuth } from '@/context/Authcontext';
 
-import LibraryPage from '../../public/Library.png';
-import GameDetailsPage from '../../public/GameDetails.png'
-import ChatroomPage from '../../public/ChatRoom.png';
-import JournalPage from '../../public/JournalPage.png';
-
 export default function Home() {
   const router = useRouter();
   const [mounted, setMounted] = useState(false);
@@ -109,7 +104,7 @@ export default function Home() {
           <div className={classes.imageReverseContainer} >
 
             <Image
-              src={GameDetailsPage.src}
+              src="/ContactBackground.jpg"
               alt="Game Details Page"
               className={classes.image}
               width={650}
