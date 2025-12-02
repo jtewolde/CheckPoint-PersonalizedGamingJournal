1e95c4cf663870c2b8168a4ab61dcc9717b4fdcc
Joseph <jtewold1@msudenver.edu>
2025-12-02 11:26:54 -0700

    Created Trending Games section for landing page that displays trending games from API Route on Carousel on hero section.
    Implemented trending component to landing page successfully. Needs more styling for game card that shows title of game and other info. Need to fix hover effect

--- checkpoint-gamingjournal/src/components/TrendingGamesSection/TrendingGamesSection.tsx
+++ checkpoint-gamingjournal/src/components/TrendingGamesSection/TrendingGamesSection.tsx
@@ -1,8 +1,11 @@
 'use client'
 
 import { useEffect, useState } from "react";
-import { Card } from "@mantine/core";
-import Carousel from "react-multi-carousel";
+import { useRouter } from "next/navigation";
+
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 import Image from "next/image";
 
 import classes from './TrendingSection.module.css';
@@ -14,6 +17,7 @@ export default function TrendingGamesSection() {
     const [trendingGames, setTrendingGames] = useState<any[]>([]);
     const [loading, setLoading] = useState(true);
     const [hasMounted, setHasMounted] = useState(false);
+    const router = useRouter();
 
     // Fetch trending games data from backend API on component mount
     useEffect(() => {
@@ -42,8 +46,7 @@ export default function TrendingGamesSection() {
     const trendingResponsive = {
         desktop: {
             breakpoint: { max: 3000, min: 1024 },
-            items: 1, // Show three slides at a time for desktop
-            slidesToSlide: 1, // Number of slides to scroll at once
+            items: 1, // Show one slide at a time for desktop
         },
     }
 
@@ -58,28 +61,27 @@ export default function TrendingGamesSection() {
 
             <Carousel
             responsive={trendingResponsive}
+            autoPlay={true}
+            autoPlaySpeed={6000}
             infinite
             keyBoardControl
             slidesToSlide={1}
             >
                 {trendingGames.map((game) => (
-                    <Card key={game.id} className={classes.gameCard}>
-                        <Image
+                    <div key={game.id} className={classes.gameCard}>
+                        <img
                             src={game.cover
                                 ? `https:${game.cover.url.replace('t_thumb', 't_1080p')}`
                                 : PlaceHolderImage.src
                             }
                             alt={game.name}
-                            width={200}
-                            height={250}
                             className={classes.gameImage}
+                            onClick={() => router.push(`/games/${game.id}`)} 
                         />
                         <h3 className={classes.gameTitle}>{game.name}</h3>
-                    </Card>
+                    </div>
                 ))}
             </Carousel>
         </div>
     )
 }
-
-
