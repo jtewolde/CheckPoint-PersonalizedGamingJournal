c863e1f59c505a3ea93142bded0c5cb87f8167db
Joseph <jtewold1@msudenver.edu>
2026-03-06 19:31:51 -0700

    Refactored trending games carousel to use Swiper instead of react carousel. Now using gameCard component for reusability

--- checkpoint-gamingjournal/src/components/TrendingGamesCarousel/TrendingGamesCarousel.tsx
+++ checkpoint-gamingjournal/src/components/TrendingGamesCarousel/TrendingGamesCarousel.tsx
@@ -2,19 +2,21 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { Badge } from "@mantine/core";
 
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/thumbs';
+import 'swiper/css/free-mode';
 
 import classes from './TrendingCarousel.module.css';
 
-import PlaceHolderImage from '../../../public/no-cover-image.png';
-
 import { CircleArrowRight } from "lucide-react";
 
-import { IconBrandXbox, IconFileDescription, IconBook, IconSwords, IconBrush, IconUsersGroup, IconDeviceGamepad2, 
-IconRating18Plus, IconIcons, IconDevicesPc, IconBrandGoogle, IconDeviceNintendo, IconBrandAndroid, IconBrandApple } from '@tabler/icons-react';
+import { IconBrandXbox, IconIcons, IconDevicesPc, IconBrandGoogle, IconBrandWindows, IconDeviceNintendo, IconBrandAndroid, IconBrandApple } from '@tabler/icons-react';
+import GameCard from "../GameCard/GameCard";
 
 export default function TrendingGamesCarousel() {
     // States to hold trending games data and loading status
@@ -46,41 +48,27 @@ export default function TrendingGamesCarousel() {
         setHasMounted(true);
     }, []);
 
-    // Responsive settings for the carousel
-    const trendingResponsive = {
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 1, // Show one slide at a time for desktop
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 640 },
-            items: 1,
-        },
-        mobile: {
-            breakpoint: { max: 640, min: 0 },
-            items: 1,
-        },
-    }
-
 
     // Function to retrieve logos for different platforms that games can be on
     const getPlatformIcon = (platformAbbreviation: string) => {
 
-        if (platformAbbreviation.includes("Series X|S")) return <IconBrandXbox size={20} />;
+        if (platformAbbreviation.includes("Series X|S")) return <IconBrandXbox size={25} />;
 
-        if (platformAbbreviation.includes("PS")) return <IconIcons size={20} />;
+        if (platformAbbreviation.includes("PS")) return <IconIcons size={25} />;
 
         if (platformAbbreviation.includes("PC") || platformAbbreviation.includes("win"))
-        return <IconDevicesPc size={20} />;
+        return <IconBrandWindows size={25} />;
 
-        if (platformAbbreviation.includes("Switch")) return <IconDeviceNintendo size={20} />;
+        if (platformAbbreviation.includes("Linux")) return <IconDevicesPc size={25} />
 
-        if (platformAbbreviation.includes("And")) return <IconBrandAndroid size={20} />;
+        if (platformAbbreviation.includes("Switch")) return <IconDeviceNintendo size={25} />;
 
-        if (platformAbbreviation.includes("ggl")) return <IconBrandGoogle size={20} />
+        if (platformAbbreviation.includes("Android")) return <IconBrandAndroid size={25} />;
+
+        if (platformAbbreviation.includes("ggl")) return <IconBrandGoogle size={25} />
 
         if (platformAbbreviation.includes("IOS") || platformAbbreviation.includes("mac")) 
-        return <IconBrandApple size={20} />;
+        return <IconBrandApple size={30} />;
 
         return null; // fallback if no match
     };
@@ -93,67 +81,23 @@ export default function TrendingGamesCarousel() {
                 <a className={classes.viewMoreIcon} href="/search/trending"> <CircleArrowRight size={35} /> </a>
             </div>
             
-            <Carousel
-            arrows={false}
-            showDots={true}
-            responsive={trendingResponsive}
-            autoPlay={true}
-            autoPlaySpeed={6000}
-            infinite
-            keyBoardControl
-            slidesToSlide={1}
+            <Swiper
+            centeredSlides={true}
+            loop={true}
+            autoplay
+            pagination={{ clickable: true }}
+            navigation
+            modules={[Navigation, Pagination]}
+            spaceBetween={50}
+            slidesPerView={1.4}
+            className={classes.swiperContainer}
             >
                 {trendingGames.map((game) => (
-                    <div key={game.id} className={classes.gameCard}>
-                        <img
-                            src={game.cover
-                                ? `https:${game.cover.url.replace('t_thumb', 't_1080p')}`
-                                : PlaceHolderImage.src
-                            }
-                            alt={game.name}
-                            className={classes.gameImage}
-                            onClick={() => router.push(`/games/${game.id}`)} 
-                        />
-
-                        <div className={classes.gameInfo}>
-                            <h3 className={classes.gameTitle}>{game.name}</h3>
-                            
-                            <div className={classes.badgeContainer}>
-
-                                <div className={classes.genreBadges}>
-
-                                    {game.genres?.slice(0, 2).map((genre: { name: string }) => (
-                                    <Badge key={genre.name} size="md" variant="filled" color="white" radius='md' c='black'>
-                                        {genre.name}
-                                    </Badge>
-                                    ))}
-
-                                </div>
-
-                                <div className={classes.platformBadges}>
-
-                                    {game.platforms?.slice(0, 4).map((platform: { abbreviation: string }) => (
-                                    <Badge key={platform.abbreviation} size="md" variant="filled" color="#5e5c5c" radius='md' c='white' leftSection={getPlatformIcon(platform.abbreviation)}>
-                                        {platform.abbreviation}
-                                    </Badge>
-                                    ))}
-
-                                </div>
-
-                            </div>
-
-                            <div className={classes.dateRatingContainer}>
-
-                                <p className={classes.gameDate}>{game.release_dates?.[0]?.human} </p>
-                            
-                            </div>
-                            
-
-                        </div>
-                        
-                    </div>
+                    <SwiperSlide key={game.id} className={classes.gameCard}>
+                        <GameCard game={game} variant="default" key={game.id}/>
+                    </SwiperSlide>
                 ))}
-            </Carousel>
+            </Swiper>
         </div>
     )
 }
