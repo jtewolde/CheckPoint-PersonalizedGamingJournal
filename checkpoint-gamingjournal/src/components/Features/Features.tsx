a627612c94abfd5176beb34fb56325573470e63c
Joseph <jtewold1@msudenver.edu>
2026-04-18 14:42:43 -0600

    Added PlaySession image and update feature descriptions for clarity. Also removed Gemini AI Chatbot info on landing page

--- checkpoint-gamingjournal/src/components/Features/Features.tsx
+++ checkpoint-gamingjournal/src/components/Features/Features.tsx
@@ -1,6 +1,6 @@
 
-import { Pin, Notebook, Library, BotMessageSquare, Gamepad2, Smartphone} from 'lucide-react';
-import { Container, SimpleGrid, Text, ThemeIcon, Title } from '@mantine/core';
+import { Pin, Notebook, Library, BotMessageSquare, Gamepad2, Smartphone, } from 'lucide-react';
+import { SimpleGrid, Text, ThemeIcon } from '@mantine/core';
 import { Transition } from '@mantine/core';
 import { useState, useEffect } from 'react';
 
@@ -10,43 +10,43 @@ export const featuresData = [
     {
         icon: <Pin size={50} />,
         title: 'Track Your Game Progress',
-        description: 'Log milestones, achievements, and story progress, see an overview of your gaming journey.',
-        iconColor: 'pink'
+        description: 'Monitor your overall progress across games, including milestones, achievements, and completion status.',
+        iconColor: '#d813d2'
     },
     
     {
         icon: <Notebook size={50} />,
         title: 'Personalized Journal',
         description: 'Write session recaps, strategies, and personal notes to document your gaming journey.',
-        iconColor: 'blue'
+        iconColor: '#2b5ae7'
     },
 
     {
         icon: <Library size={50} />,
         title: 'Game Library Management',
         description: 'Organize your games into categories like Currently Playing, Backlog, and Completed for easy tracking.',
-        iconColor: 'red'
+        iconColor: '#e31a1a'
     },
 
     {
         icon: <BotMessageSquare size={50} />,
-        title: 'Gemini AI Powered Chat',
-        description: 'Be able to chat with the Gemini AI about your gaming progress, ask questions, and get game recommendations."',
-        iconColor: 'purple'
+        title: 'Track Play Sessions',
+        description: 'Record each gaming session with playtime, activities, and notes to build a detailed history of your gameplay.',
+        iconColor: '#8a1ddd'
     },
 
     {
         icon: <Gamepad2 size={50} />,
         title: 'Detailed Game Info',
         description: 'Access rich game details including release dates, genres, ratings, screenshots, and more to learn about your favorite titles from IGDB.',
-        iconColor: 'green'
+        iconColor: '#23e139'
     },
 
     {
         icon: <Smartphone size={50}  />,
         title: 'Responsive Design',
         description: 'Enjoy a seamless experience on both desktop and mobile devices, perfect for logging sessions on the go.',
-        iconColor: 'yellow'
+        iconColor: '#e9a41a'
     }
     
 ]
